036b845ad9f2aeda578a3f4f7f1d6e42b548f5af
muckity <andrewgoldstein@hotmail.com>
2020-06-09 10:16:38 -0500

    fix tesseract install

--- recieptRead.py
+++ recieptRead.py
@@ -1,9 +1,8 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+from PIL import Image
 import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+im = Image.open("reciept.jpg")
 
-print(pytesseract.image_to_string(Image.open('reciept.jpg')))
-
+text = pytesseract.image_to_string(im)
+print(text)

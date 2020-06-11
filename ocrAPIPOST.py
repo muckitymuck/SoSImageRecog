3ebf4eba8109ffd868b438439f47196cde00d6c3
muckity <andrewgoldstein@hotmail.com>
2020-06-11 14:29:01 -0500

    url posting fixed

--- ocrAPIPOST.py
+++ ocrAPIPOST.py
@@ -20,7 +20,7 @@ def ocr_space_file(filename, overlay=False, api_key='7528f1b76188957', language=
     payload = {'isOverlayRequired': overlay,
                'apikey': api_key,
                'language': language,
-               'istable': istable
+               'istable': istable,
                }
     with open(filename, 'rb') as f:
         r = requests.post('https://api.ocr.space/parse/image',
@@ -30,9 +30,10 @@ def ocr_space_file(filename, overlay=False, api_key='7528f1b76188957', language=
     return r.content.decode()
 
 
-def ocr_space_url(url, overlay=False, api_key='7528f1b76188957', language='eng', isTable=True):
+def ocr_space_url(url, overlay=False, api_key='7528f1b76188957', language='eng', istable=True):
     """ OCR.space API request with remote file.
         Python3.5 - not tested on 2.7
+
     :param url: Image url.
     :param overlay: Is OCR.space overlay required in your response.
                     Defaults to False.
@@ -42,12 +43,14 @@ def ocr_space_url(url, overlay=False, api_key='7528f1b76188957', language='eng',
                     List of available language codes can be found on https://ocr.space/OCRAPI
                     Defaults to 'en'.
     :return: Result in JSON format.
+    :param isTable: parses output as a table
     """
 
     payload = {'url': url,
                'isOverlayRequired': overlay,
                'apikey': api_key,
                'language': language,
+               'istable': istable,
                }
     r = requests.post('https://api.ocr.space/parse/image',
                       data=payload,
@@ -55,5 +58,5 @@ def ocr_space_url(url, overlay=False, api_key='7528f1b76188957', language='eng',
     return r.content.decode()
 
 
-ocr_space_file('receipt2.jpg')
 print(ocr_space_file('receipt2.jpg'))
+print(ocr_space_url('https://ocr.space/Content/Images/receipt-ocr-original.jpg'))

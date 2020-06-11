a5c7563e9bb6a38db7d40ef6302ce6174b6c022d
muckity <andrewgoldstein@hotmail.com>
2020-06-11 13:42:31 -0500

    import requests install

--- ocrapi.py
+++ ocrapi.py
@@ -1,12 +1,15 @@
 # OCR.SPACE API KEY 7528f1b76188957
 
 import ocrspace
-
+import requests
 
 # Or if you have an API key or desired language, pass those:
 # api = ocrspace.API('Insert key here', ocrspace.Language.Croatian)
 
 api = ocrspace.API('7528f1b76188957')
 
-api.ocr_file('receipt2.jpg')
+# Local File
+# api.ocr_file('receipt2.jpg')
 
+# URL
+api.ocr_url('https://ocr.space/Content/Images/receipt-ocr-original.jpg')

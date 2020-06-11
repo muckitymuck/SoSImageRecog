c2d073036782a0b3b1249fed80592925348020ab
muckity <andrewgoldstein@hotmail.com>
2020-06-11 13:21:29 -0500

    fix tesseract install

--- ocrapi.py
+++ ocrapi.py
@@ -1,2 +1,12 @@
 # OCR.SPACE API KEY 7528f1b76188957
 
+import ocrspace
+
+
+# Or if you have an API key or desired language, pass those:
+# api = ocrspace.API('Insert key here', ocrspace.Language.Croatian)
+
+api = ocrspace.API('7528f1b76188957')
+
+api.ocr_file('receipt2.jpg')
+
